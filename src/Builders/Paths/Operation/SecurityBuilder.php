e6f0068cb03b19aee8aaa3c2d173a8b6152640eb
Arron King <arron.king@arrow-web.co.uk>
2021-08-19 16:40:51 +0100

    When security = '' return a null scheme

--- src/Builders/Paths/Operation/SecurityBuilder.php
+++ src/Builders/Paths/Operation/SecurityBuilder.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace Vyuldashev\LaravelOpenApi\Builders\Paths\Operation;
 
+use Vyuldashev\LaravelOpenApi\RouteInformation;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityRequirement;
 use Vyuldashev\LaravelOpenApi\Attributes\Operation as OperationAttribute;
-use Vyuldashev\LaravelOpenApi\RouteInformation;
 
 class SecurityBuilder
 {
@@ -14,6 +13,10 @@ class SecurityBuilder
             ->filter(static fn (object $attribute) => $attribute instanceof OperationAttribute)
             ->filter(static fn (OperationAttribute $attribute) => isset($attribute->security))
             ->map(static function (OperationAttribute $attribute) {
+                // return a null scheme if the security is set to ''
+                if ($attribute->security === '') {
+                    return SecurityRequirement::create()->securityScheme(null);
+                }
                 $security = app($attribute->security);
                 $scheme = $security->build();
 
