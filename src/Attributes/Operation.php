019d11e5bf5d9f1eeeac6acf48f7f3e52e6bb3e9
qwasyx0 <43924784+qwasyx0@users.noreply.github.com>
2022-04-07 18:31:55 +0200

    Update Operation.php

--- src/Attributes/Operation.php
+++ src/Attributes/Operation.php
@@ -12,15 +12,13 @@ class Operation
     public ?string $id;
 
     /** @var array<string> */
-    public array $security;
-
-    /** @var array<string> */
     public array $tags;
 
     public ?string $security;
 
     public ?string $method;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     /**
      * @param  string|null  $id
@@ -34,12 +32,22 @@ class Operation
 =======
     public function __construct(string $id = null, array $tags = [], array $security = [], string $method = null)
 >>>>>>> 91eae28 (Add security to Operation Attribute)
+=======
+    /**
+     * @param string|null $id
+     * @param array $tags
+     * @param \Vyuldashev\LaravelOpenApi\Factories\SecuritySchemeFactory|string|null $security
+     * @param string|null $method
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $id = null, array $tags = [], string $security = null, string $method = null)
+>>>>>>> 250ae9f (Update Operation.php)
     {
         $this->id = $id;
         $this->tags = $tags;
-        $this->security = $security;
         $this->method = $method;
 
+<<<<<<< HEAD
         if ($security === '') {
             //user wants to turn off security on this operation
             $this->security = $security;
@@ -53,6 +61,14 @@ class Operation
             if (! is_a($this->security, SecuritySchemeFactory::class, true)) {
                 throw new InvalidArgumentException(
                     sprintf('Security class is either not declared or is not an instance of %s', SecuritySchemeFactory::class)
+=======
+        if ($security) {
+            $this->security = class_exists($security) ? $security : app()->getNamespace() . 'OpenApi\\SecuritySchemes\\' . $security . 'SecurityScheme';
+
+            if (!is_a($this->security, SecuritySchemeFactory::class, true)) {
+                throw new InvalidArgumentException(
+                    sprintf('Security class is either not declared or is not an instance of [%s]', SecuritySchemeFactory::class)
+>>>>>>> 250ae9f (Update Operation.php)
                 );
             }
         }
